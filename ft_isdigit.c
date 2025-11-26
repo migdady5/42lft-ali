d70073d0288ff14506630d4e7ed3e2f53875faae
Ali migdady <amigdadi@c1r5s2.irbid.42.school>
2025-11-26 18:57:54 +0300

    a

---
+++ ft_isdigit.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amigdadi <amigdadi@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/25 21:58:00 by amigdadi          #+#    #+#             */
+/*   Updated: 2025/11/26 18:54:19 by amigdadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+/*int	main(void)
+{
+	int a = '5';
+	int b = 'x';
+
+	printf("ft_isdigit('%c') = %d\n", a, ft_isdigit(a));
+	printf("ft_isdigit('%c') = %d\n", b, ft_isdigit(b));
+	return (0);
+}*/
