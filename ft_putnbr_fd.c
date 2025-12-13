9d15fbae48f849488634f91c5c54ec9e0679f535
Ali <anasmig_1991@hotmail.com>
2025-12-14 00:05:31 +0300

    all fun not done norm

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amigdadi <amigdadi@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/14 00:03:19 by amigdadi          #+#    #+#             */
+/*   Updated: 2025/12/14 00:03:19 by amigdadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long	num;
+
+	num = (long)n;
+	if (num < 0)
+	{
+		ft_putchar_fd('-', fd);
+		num = -num;
+	}
+	if (num >= 10)
+		ft_putnbr_fd((int)(num / 10), fd);
+	ft_putchar_fd((char)((num % 10) + '0'), fd);
+}
---
+++ ft_strmapi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amigdadi <amigdadi@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/13 23:54:50 by amigdadi          #+#    #+#             */
+/*   Updated: 2025/12/13 23:54:50 by amigdadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*new_str;
+	size_t	i;
+
+	if (!s || !f)
+		return (NULL);
+	new_str = (char *)malloc(ft_strlen(s) + 1);
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		new_str[i] = f((unsigned int)i, s[i]);
+		i++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
